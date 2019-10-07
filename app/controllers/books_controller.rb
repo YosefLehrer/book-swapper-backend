72f30915ae0af842088d2e3747e6f97669cb0434
yoseflehrer <yolehrer@gmail.com>
2019-10-06 22:42:56 -0400

    added nyt bestsellers list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,26 @@
 require 'net/http'
 class BooksController < ApplicationController
+    def create_book_object(book)
+        {
+            title: book["volumeInfo"]["title"],
+            author: book["volumeInfo"]["authors"] ? book["volumeInfo"]["authors"][0] : "Author not found",
+            img: book["volumeInfo"]["imageLinks"] ? book["volumeInfo"]["imageLinks"]["thumbnail"]: "https://cdn2.iconfinder.com/data/icons/unigrid-phantom-multimedia-vol-6/60/020_256_rules_do_not_list_book_history_education_study_learn_knowledge-128.png",
+            description: book["volumeInfo"]["description"] ? book["volumeInfo"]["description"] : "Description is not available",
+            publisheddate: book["volumeInfo"]["publishedDate"],
+            pagecount: book["volumeInfo"]["pageCount"],
+            rating: book["volumeInfo"]["averageRating"],
+            infolink: book["volumeInfo"]["infoLink"],
+            googleid: book["id"]
+        }
+    end
+
     def search
         searchTerm = params[:searchTerm]
         encodedSearchTerm = URI.escape(searchTerm)
         uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{encodedSearchTerm}")
         response = Net::HTTP.get_response(uri)
         body = JSON.parse(response.body)
-        parsedArrayOfBooks = body["items"].map do |book|
-            {
-                title: book["volumeInfo"]["title"],
-                author: book["volumeInfo"]["authors"] ? book["volumeInfo"]["authors"][0] : "Author not found",
-                # isbn: book["volumeInfo"]["industryIdentifiers"].find {|identifier| identifier["type"] === "ISBN_10" || identifier["type"] === "OTHER"}["identifier"]
-                img: book["volumeInfo"]["imageLinks"] ? book["volumeInfo"]["imageLinks"]["thumbnail"]: "https://cdn2.iconfinder.com/data/icons/unigrid-phantom-multimedia-vol-6/60/020_256_rules_do_not_list_book_history_education_study_learn_knowledge-128.png",
-                description: book["volumeInfo"]["description"] ? book["volumeInfo"]["description"] : "Description is not available",
-                publisheddate: book["volumeInfo"]["publishedDate"],
-                pagecount: book["volumeInfo"]["pageCount"],
-                rating: book["volumeInfo"]["averageRating"],
-                infolink: book["volumeInfo"]["infoLink"],
-                googleid: book["id"]
-            }
-        end
+        parsedArrayOfBooks = body["items"].map {|book| self.create_book_object(book)}
         render :json => parsedArrayOfBooks
     end
 
@@ -34,6 +35,25 @@ class BooksController < ApplicationController
     end
 
     def nyt_bestsellers
-        ENV["NYT_BESTSELLERS"]
+        uri = URI.parse("https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=#{ENV["NYT_BESTSELLERS"]}")
+        response = Net::HTTP.get_response(uri)
+        body = JSON.parse(response.body)
+        list = body["results"]["lists"]
+        
+        fiction = list.find{|obj| obj["list_name_encoded"] == "hardcover-fiction"}["books"].map do |book|
+            uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{book["title"]}")
+            response = Net::HTTP.get_response(uri)
+            body = JSON.parse(response.body)
+            self.create_book_object(body["items"][0])
+        end
+
+        hardcover_nonfiction = list.find{|obj| obj["list_name_encoded"] == "hardcover-nonfiction"}["books"].map do|book|
+            uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{book["title"]}")
+            response = Net::HTTP.get_response(uri)
+            body = JSON.parse(response.body)
+            self.create_book_object(body["items"][0])
+        end
+
+        render json: {fiction: fiction, nonfiction: hardcover_nonfiction}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :owned_books, :books, :users
   
   get "/books/search/q=:searchTerm", to: "books#search"
+  get "/nyt_bestsellers", to: "books#nyt_bestsellers"
   get "/user_library", to: "users#user_library"
   get '/autologin', to: 'auth#autologin'
-
+  
   post "/login", to: "auth#login"
   post "/offer_trade", to: "trades#find_or_create"
   post "accept_trade", to: "trades#accept_trade"
